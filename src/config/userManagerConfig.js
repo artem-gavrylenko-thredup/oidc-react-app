55c11a2ac6506d52b67fd7ed5e6f57f9258a52e0
Artem Gavrylenko <artem.gavrylenko@thredup.com>
2019-10-25 16:39:57 +0300

    add react-oidc

---
+++ src/config/userManagerConfig.js
@@ -0,0 +1,7 @@
+export default {
+    authority: process.env.REACT_APP_AUTH_URL,
+    client_id: process.env.REACT_APP_IDENTITY_CLIENT_ID,
+    redirect_uri: process.env.REACT_APP_REDIRECT_URL,
+    response_type: process.env.REACT_APP_RESPONSE_TYPE,
+    scope: process.env.REACT_APP_SCOPE,
+};
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import { BrowserRouter as Router , Route, Switch } from "react-router-dom";
+import { makeAuthenticator, makeUserManager, Callback } from 'react-oidc';
+
 import App from './App';
+
+import userManagerConfig from './config/userManagerConfig';
+
+const userManager = makeUserManager(userManagerConfig);
+const AppWithAuth = makeAuthenticator({
+    userManager,
+    signinArgs: {
+        state: {
+            foo: 15
+        }
+    }
+})(App);
 
 const Routes = () => (
     <Router>
         <Switch>
-            <Route path="/" component={App} />
+            <Route
+                path="/callback"
+                render={routeProps => (
+                    <Callback
+                        onSuccess={user => {
+                            // `user.state` will reflect the state that was passed in via signinArgs.
+                            console.log('user', user);
+                            routeProps.history.push('/')
+                        }}
+                        userManager={userManager}
+                    />
+                )}
+            />
+            <Route path="/" component={AppWithAuth} />
         </Switch>
     </Router>
 );
